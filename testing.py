Fixed softmax issues in testing.

testing.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+import torch.nn.functional as F
 from model import vgg11
 from torch.utils.data import DataLoader
 import torchvision.models as models
@@ -59,11 +60,11 @@ def testing(
 
             output = model(images)
             if output.shape[0] == 1:
-                output = torch.sigmoid(output)
+                output = F.sigmoid(output)
                 y_probs.append(output.cpu().numpy().item())
                 predicted = 1 if output > 0.5 else 0
             else:
-                output = torch.softmax(output)
+                output = F.softmax(output, dim=1)
                 y_probs.append(output.cpu().numpy())
                 _, predicted = torch.max(output.data, 1)
             y_preds.append(predicted)

training.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from dataset import PowderDataset
@@ -254,7 +255,7 @@ class BinaryTrainer(Trainer):
             output = (
                 output.squeeze()
             )  # gets rid of the second dimension which is always 1
-            output = torch.sigmoid(output)  # finds the probability from the logit
+            output = F.sigmoid(output)  # finds the probability from the logit
             predicted = torch.tensor([1 if x >= 0.5 else 0 for x in output]).to(
                 self.device
             )
@@ -302,7 +303,7 @@ class BinaryTrainer(Trainer):
                 output = (
                     output.squeeze()
                 )  # gets rid of the second dimension which is always 1
-                output = torch.sigmoid(output)  # finds the probability from the logit
+                output = F.sigmoid(output)  # finds the probability from the logit
                 predicted = torch.tensor([1 if x >= 0.5 else 0 for x in output]).to(
                     self.device
                 )